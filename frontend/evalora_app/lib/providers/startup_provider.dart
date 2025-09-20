7103424c6c2861cbbe7d31143bb896419430fac8
Tharun Ganapathi <tharunganapathik@gmail.com>
2025-09-20 21:39:38 +0530

    Reorg folders

--- frontend/evalora_app/lib/providers/startup_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../models/startup.dart';
-import '../api/mock_service.dart';
-
-
-final startupsProvider = FutureProvider<List<Startup>>((ref) async {
-return await mockService.fetchStartups();
-});
